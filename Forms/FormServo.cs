e7ccacc8003b17bc6f8c7bbcf3e16ba41655d8fb
DangTat <dangtat.n20@gmail.com>
2024-01-25 08:42:18 +0700

    tat
    dfdf

--- Forms/FormServo.cs
+++ Forms/FormServo.cs
@@ -16,57 +16,143 @@ namespace Learn_0801_24.Forms
     {
         public Plc _plc;
         Timer Timer_Servo=new Timer();
-        int Time_update = 100;
+        int Time_update_sv = 100;
         
-            
+
         public FormServo()
         {
             InitializeComponent();
             //LoadTheme();
-            Timer_Servo.Interval = Time_update;
+            Timer_Servo.Interval = Time_update_sv;
             Timer_Servo.Start();
             Timer_Servo.Tick += Timer_Servo_Tick;
         }
-        public struct DB1
-        {
-            public bool a;
-            public short b;
-            public int c;
-            public double d;
-
-        }
+        
         private void Formservo_Load(object sender, EventArgs e)
         {
-            
+            Timer_Servo.Interval = Time_update_sv;
+            Timer_Servo.Start();
+            Timer_Servo.Tick += Timer_Servo_Tick;
         }
         public void Timer_Servo_Tick(object sender, EventArgs e)
         {
-           //if (_plc.IsConnected)
-           // {
-           //     DB1 db1 = (DB1)_plc.ReadStruct(typeof(DB1), 1);
-           //     tbJogSpeed.Text = db1.b.ToString();
-           //     tbHomeSpeed.Text = db1.c.ToString();
-           //     tbVelocity.Text = Convert.ToString(db1.d).ToString();
+            if (_plc.IsConnected)
+            {
+             bool DB1_DBX0_0 = (bool)_plc.Read("DB1.DBX0.0");
+                if (DB1_DBX0_0)
+                {
+                    sBtnHomeServo.CheckState = CheckState.Checked;
+                    btnHomeServo.BackColor = Color.Green;
+                }
+                else
+                    sBtnHomeServo.CheckState = CheckState.Unchecked;
+                    btnHomeServo.BackColor = Color.Red;
 
-
-           // }
-
+            }
+        }
+        public struct DB16
+        {
+            public bool a;
+            public bool b;
         }
 
-        private void btnOnServo_Click(object sender, EventArgs e)
+
+        #region // Home servo
+        private void btnHomeServo_MouseDown(object sender, MouseEventArgs e)
         {
             if (_plc.IsConnected)
             {
-                _plc.WriteBit(DataType.DataBlock, 1, 0, 4, 1);
+                _plc.WriteBit(DataType.DataBlock, 1, 0, 4, 1);// home servo
             }
         }
 
-        private void btnOffServo_Click(object sender, EventArgs e)
+        private void btnHomeServo_MouseUp(object sender, MouseEventArgs e)
         {
             if (_plc.IsConnected)
             {
-                _plc.WriteBit(DataType.DataBlock, 1, 0, 4, 0);
+                _plc.WriteBit(DataType.DataBlock, 1, 0, 4, 0);// home servo
             }
+        }
+
+        #endregion
+
+        private void sBtnOnServoLift_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (sBtnOnServoLift.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo1_1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sBtnOnServo1_1.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo1_2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            if (sBtnOnServo1_2.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo2_1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sBtnOnServo2_1.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo2_2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sBtnOnServo2_2.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo3_1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sBtnOnServo3_1.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
+        }
+
+        private void sBtnOnServo3_2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sBtnOnServo3_2.Checked)
+            {
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 1); // ( DB3.DBX0.0, BIT WRITE XUỐNG ) }
+
+            }
+            else
+                _plc.WriteBit(DataType.DataBlock, 3, 0, 4, 0);
         }
 
         
